70799d2026111f78303c2c1483bad915eb7ddae4
Zhang Yu <yzhang5@insa-20656>
2024-11-26 14:25:56 +0100

    init

---
+++ feedbackMs/src/main/java/fr/insa/microservice/feedbackMs/FeedbackMsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.microservice.feedbackMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackMsApplication.class, args);
+	}
+
+}
---
+++ feedbackMs/src/test/java/fr/insa/microservice/feedbackMs/FeedbackMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.microservice.feedbackMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
