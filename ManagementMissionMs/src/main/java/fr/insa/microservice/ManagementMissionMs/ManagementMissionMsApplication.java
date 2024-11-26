bcb9f891bc435c6930a3dbeaa0b58f0bd5158cce
Zhang Yu <yzhang5@insa-20656>
2024-11-26 14:23:05 +0100

    init managementMissionMS

---
+++ ManagementMissionMs/src/main/java/fr/insa/microservice/ManagementMissionMs/ManagementMissionMsApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.microservice.ManagementMissionMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementMissionMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementMissionMsApplication.class, args);
+	}
+
+}
---
+++ ManagementMissionMs/src/test/java/fr/insa/microservice/ManagementMissionMs/ManagementMissionMsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.microservice.ManagementMissionMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementMissionMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
