b7760b9cc5183fa5aed4379b1a8787b663a20149
ZYJ <yongjiazeng@yongjias-MacBook-Air.local>
2024-11-27 20:45:44 +0100

    update bdd

--- ManagementMissionMs/src/main/java/fr/insa/microservice/ManagementMissionMs/controller/managementMissionSQL.java
+++ ManagementMissionMs/src/main/java/fr/insa/microservice/ManagementMissionMs/controller/managementMissionSQL.java
@@ -1,7 +1,6 @@
 package fr.insa.microservice.ManagementMissionMs.controller;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,16 +15,21 @@ import fr.insa.microservice.ManagementMissionMs.model.Mission;
 
 public class managementMissionSQL {
 
- public static List<Mission> getAllMission() {
+    public static List<Mission> getAllMission() {
         List<Mission> missions = new ArrayList<>();
-        String query = "SELECT * FROM Mission";
+        String query = """
+                SELECT m.idmission, m.idDemandeur, m.idBenevole, m.state, c.comment
+                FROM Mission m
+                LEFT JOIN Comment c ON m.idmission = c.idmission
+                """;
+    
         ConnectionJavaMySQL connectionManager = new ConnectionJavaMySQL();
         Connection connection = connectionManager.getConnection();
-
+    
         try (
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(query)) {
-
+    
             while (resultSet.next()) {
                 Mission mission = new Mission(
                         resultSet.getInt("idmission"),
@@ -36,18 +40,24 @@ public class managementMissionSQL {
                 );
                 missions.add(mission);
             }
-
+    
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+    
         return missions;
     }
+    
 
     
     public static Mission getMissionById(int idMission) {
         Mission mission = null;
-        String query = "SELECT * FROM Mission WHERE idmission = ?";
+        String query ="""
+            SELECT m.idmission, m.idDemandeur, m.idBenevole, m.state, c.comment
+            FROM Mission m
+            LEFT JOIN Comment c ON m.idmission = c.idmission
+            WHERE m.idmission = ?
+            """;
         ConnectionJavaMySQL connectionManager = new ConnectionJavaMySQL();
         Connection connection = connectionManager.getConnection();
 
@@ -87,19 +97,26 @@ public class managementMissionSQL {
         }
     }
 
-public static void deleteMission(int idMission) {
-   
-    ConnectionJavaMySQL connectionManager = new ConnectionJavaMySQL();
-    Connection connection = connectionManager.getConnection();
-    String query = "DELETE FROM Mission WHERE idmission = ?";
-    
-    try ( PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-        preparedStatement.setInt(1, idMission);
-        preparedStatement.executeUpdate();
-    }catch(SQLException e){
-        e.printStackTrace();
+    public static void deleteMission(int idMission) {
+        ConnectionJavaMySQL connectionManager = new ConnectionJavaMySQL();
+        Connection connection = connectionManager.getConnection();
+        String query1 = "DELETE FROM Mission WHERE idmission = ?";
+        String query2 = "DELETE FROM Comment WHERE idmission = ?";
+        
+        try {
+            try(PreparedStatement preparedStatement2 = connection.prepareStatement(query2))
+            { preparedStatement2.setInt(1, idMission);
+                preparedStatement2.executeUpdate();    
+            }
+            try(PreparedStatement preparedStatement1 = connection.prepareStatement(query1))
+            { preparedStatement1.setInt(1, idMission);
+                preparedStatement1.executeUpdate();
+            }
+           
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
-}
 
 
 public static void assignMission(int idMission, int idBenevole) {
