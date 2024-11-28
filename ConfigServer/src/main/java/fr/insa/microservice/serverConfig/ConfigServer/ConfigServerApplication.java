0023788f4d3a7507e20a8a5386411ad96017d3ae
Zhang Yu <yzhang5@insa-11269>
2024-11-28 13:34:06 +0100

    test

--- ConfigServer/src/main/java/fr/insa/microservice/serverConfig/ConfigServer/ConfigServerApplication.java
+++ ConfigServer/src/main/java/fr/insa/microservice/serverConfig/ConfigServer/ConfigServerApplication.java
@@ -2,7 +2,8 @@ package fr.insa.microservice.serverConfig.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
