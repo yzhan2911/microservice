46a783069926f00baef6e1b89b446065bef5ae21
Zhang Yu <yzhang5@insa-11269>
2024-11-28 13:59:21 +0100

    test client config

---
+++ ConfigClientMSPersonne/src/main/java/fr/insa/microservice/serverConfig/ConfigClientMSPersonne/clientConfigRessource.java
@@ -0,0 +1,21 @@
+package fr.insa.microservice.serverConfig.ConfigClientMSPersonne;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class clientConfigRessource {
+	@Value("${server.port}")
+	private String serverPort;
+	@Value("${db.connection}")
+	private String typeConnectionDeDB;
+	@Value("${db.host}")
+	private String dbHost;
+	@Value("${db.port}")
+	private String dbPort;
+	@GetMapping("/information")
+	public String getAll() {
+		return "serverPort:"+serverPort+"\n"+"typeConnectionDeDB :"+typeConnectionDeDB+"\n"+"dbHost:"+"dbHost"+"\n"+"dbPort:"+dbPort;
+	}
+}
